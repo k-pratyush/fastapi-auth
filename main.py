b2727fd34a9b467804e850c6d9a96399725c493d
Pratyush Kerhalkar <pratyushkerhalkar@gmail.com>
2020-04-14 19:38:25 +0530

    added protected-route and user verification

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
 import uvicorn
 import mongoengine as db
 import bcrypt
 import jwt
+from http.cookies import SimpleCookie
 import json
 import os
 
@@ -16,6 +17,11 @@ class User(db.Document):
     username = db.StringField(required=True)
     email = db.EmailField(required=True, unique=True)
     password = db.StringField(required=True)
+
+class Post(db.Document):
+    meta = {'allow_inheritance': True}
+    text = db.StringField()
+    author = db.EmailField(required=True)
 
 @app.on_event('startup')
 def startup():
@@ -33,9 +39,9 @@ async def home():
         }
 
 @app.post('/signup')
-def signup(user_data):
+async def signup(request: Request, response: Response):
     try:
-        data = json.loads(user_data)
+        data = json.loads(await request.body())
         password = data['password'].encode('utf8')
         username = data['username']
         email = data['email']
@@ -43,9 +49,13 @@ def signup(user_data):
         try:
             user = User(username=username, password=hashed, email=email)
             user.save()
-            token = jwt.encode({"email": email}, os.environ.get('secret'), algorithm='HS256')
+            token = jwt.encode({"email": email}, os.environ.get(
+                'secret'), algorithm='HS256').decode("utf-8")
+            response.set_cookie(
+                key="token", value=f"Bearer {token}", httponly=True)
             return {
-                "success": True
+                "success": True,
+                "token": token
             }
         except db.errors.NotUniqueError:
             return {
@@ -59,8 +69,8 @@ def signup(user_data):
             }
 
 @app.post('/login')
-def login(user_data):
-    data = json.loads(user_data)
+async def login(request: Request, response: Response):
+    data = json.loads(await request.body())
     try:
         username = data['username']
         password = data['password']
@@ -70,7 +80,9 @@ def login(user_data):
             if bcrypt.checkpw(password.encode('utf-8'),
                                 user[0].password.encode('utf-8')):
                 token = jwt.encode({"email": email},
-                                   os.environ.get('secret'), algorithm='HS256')
+                                   os.environ.get('secret'), algorithm='HS256').decode("utf-8")
+                response.set_cookie(
+                    key="token", value=f"Bearer {token}", httponly=True)
                 return {
                     "success": True,
                     "token": token
@@ -87,3 +99,43 @@ def login(user_data):
             }
     except KeyError:
         print('Incomplete form')
+
+
+@app.post('/protected-route')
+async def get_protected_route(request: Request):
+    try:
+        text = json.loads(await request.body())
+        print(text)
+        token = request.cookies.get('token')
+        token = token.split(" ")[1]
+        data = jwt.decode(token, os.environ.get('secret'), algorithms=['HS256'])
+        user = User.objects(email = data['email'])
+        if user:
+            try:
+                newPost = Post(text=text['text'], author=user[0].email)
+                newPost.save()
+                return {
+                    "success": True,
+                    "message": "You are authenticated",
+                    "text": text['text']
+                }
+            except:
+                return {
+                    "success": False,
+                    "message": "there was an error posting"
+                }
+        else:
+            return {
+                "success": False,
+                "message": "You are not authenticated"
+            }
+    except KeyError:
+        return {
+            "success": False,
+            "message": "Incomplete form"
+        }
+    except AttributeError:
+        return {
+            "success": False,
+            "message": "You are not authenticated"
+        }
